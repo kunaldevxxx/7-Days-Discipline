05af10aeb88a5a4f7df668e772645863ec93899f
Kunal Khare <136145336+kunaldevxxx@users.noreply.github.com>
2024-01-09 22:55:41 +0530

    NULL&UNDEFINED QUESTION

---
+++ NULL&UNDEFINED.js
@@ -0,0 +1,4 @@
+// null and undefined in javascript ?and diffrence b/w them 
+//NULL = you try to find something but not able to find
+//undefined = if you define variable but not given value 
+var a;                                          //undefined
