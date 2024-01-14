c9361ca931492a3615a4477fa2413f1eb7313fc1
Kunal Khare <136145336+kunaldevxxx@users.noreply.github.com>
2024-01-14 21:32:50 +0530

    USESTRICT QUESTION

---
+++ QUESTIONS.js
@@ -0,0 +1,4 @@
+// make an array of objects representingv item ,filter the item on a specific condition (eg. item with certain property value) and
+// transform the filtered data into new format
+//given an array of objects with a numerical property ,implement a function to sort the objects based on property . additionaly 
+//add a ranking to each object based on its position in the sorted array
---
+++ usestrict.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var a=1;
+//variable declaration
+//can't delete the variable 
+//can't make same parameter in function
+
+
